873844fa51a86920f7b8be31bf497b99316998e7
bochao <bochao.wang@savvyuni.com>
2020-01-19 02:05:50 -0500

    feat(hackerrank): array questions

--- hackerrank/array.py
+++ hackerrank/array.py
@@ -16,12 +16,60 @@ def rotLeft(a: list, d: int):
     n_pos = d % len(a)
     return a[n_pos::] + a[:n_pos:]
 
+
 def minimumBribes(q):
     # New Year Chaos
-    
+    # if len(q) < 2:
+    #     return 0
+    # bribes = q[0] - q[1]
+    # if bribes > 2:
+    #     return 'Too chaotic'
+    # if bribes < 0:
+    #     return minimumBribes(q[1::])
+    # return bribes + minimumBribes(q[1::])
+    is_chaotic = False
+    count = 0
+    for idx, pos in enumerate(q):
+        pn = pos - 1
+        if pn - idx > 2:
+            is_chaotic = True
+            break
+        for i in range(max(0, pn-1), idx):
+            if q[i] > pos:
+                count += 1
+    print('Too chaotic' if is_chaotic else count)
+
+
+def minimumSwaps(arr):
+    # Minimum Swaps 2
+    # Time Out!
+    # swap = 0
+    # for i, _ in enumerate(arr):
+    #     while arr[i] != i+1:
+    #         arr[i], arr[arr[i] - 1] = arr[arr[i] - 1], arr[i]
+    #         swap += 1
+    #     i += 1
+    # return swap
+    sorted_arr = sorted(arr)
+    idx_dic = {val: i for i, val in enumerate(arr)}
+    swap = 0
+    print(idx_dic)
+    for i, val in enumerate(arr):
+        correct_val = sorted_arr[i]
+        if val != correct_val:
+            idx = idx_dic[correct_val]
+            # swap array
+            arr[idx], arr[i] = arr[i], arr[idx]
+            # swap dic
+            idx_dic[val] = idx
+            idx_dic[correct_val] = i
+            swap += 1
+            print(idx_dic)
+
+    return swap
 
 
 if __name__ == '__main__':
-    print(minimumBribes([1, 2, 3, 4, 5]))
+    print(minimumSwaps([2, 1, 5, 3, 4]))
     # nums = list(range(1, 6))
     # print(nums[0])
